f8e00fab4cb70f54342cfc2f983b738de69beda2
muna <muna3828@gmail.com>
2024-02-23 22:36:32 +0600

    added our story

--- src/Page/About/About.jsx
+++ src/Page/About/About.jsx
@@ -3,8 +3,23 @@
 const About = () => {
     return (
         <div>
-            <h1 className="text-3xl text center"> About Us</h1>
-            
+            <h1 className="text-3xl text center"> Our story</h1>
+            <p className="text center">Welcome to Royal oasis, where every guest becomes part of our story. From the moment you step through our doors, you embark on a journey of luxury, comfort, and unforgettable experiences.
+
+                It all began with a vision to create a haven where travelers could escape the ordinary and immerse themselves in the extraordinary. Nestled in the heart of , our hotel boasts a rich history and a commitment to excellence that sets us apart.
+
+                As you wander through our elegant lobby, you'll be greeted by warm smiles and impeccable service. Our dedicated staff are passionate about crafting personalized experiences tailored to your every need, ensuring that your stay with us is nothing short of exceptional.
+
+                Step into our thoughtfully designed rooms and suites, where modern amenities blend seamlessly with timeless elegance. Whether you're traveling for business or pleasure, each space is carefully curated to provide the ultimate in comfort and relaxation.
+
+                Indulge your senses with culinary delights at our acclaimed restaurants, where world-class chefs create tantalizing dishes inspired by local flavors and international cuisine. Savor a gourmet meal paired with fine wines, or unwind with a signature cocktail crafted with care.
+
+                But the true essence of Royal oasis lies in the experiences we offer. Whether you're seeking adventure or relaxation, our hotel is your gateway to a world of possibilities. Dive into our sparkling pool, rejuvenate your body and mind at our spa, or explore the vibrant culture and natural beauty of [Location] with our curated tours and activities.
+
+                At Royal oasis, we believe that every moment should be cherished. That's why we go above and beyond to exceed your expectations, creating memories that will last a lifetime. From romantic getaways to family vacations and everything in between, let us be the backdrop to your next unforgettable adventure.
+
+                Come, write your own chapter in our story. Your journey begins here, at Royal oasis.</p>
+
         </div>
     );
 };
